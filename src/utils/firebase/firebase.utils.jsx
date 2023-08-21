68ee4a83af92f6a1093f1f1001769c63933f15ef
Cinerarius <koustavrakshit10b@gmail.com>
2023-08-21 12:57:49 +0530

    fixed

--- src/utils/firebase/firebase.utils.jsx
+++ src/utils/firebase/firebase.utils.jsx
@@ -21,12 +21,17 @@ import {
 } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDDU4V-_QV3M8GyhC9SVieRTDM4dbiT0Yk",
-  authDomain: "crwn-clothing-db-98d4d.firebaseapp.com",
-  projectId: "crwn-clothing-db-98d4d",
-  storageBucket: "crwn-clothing-db-98d4d.appspot.com",
-  messagingSenderId: "626766232035",
-  appId: "1:626766232035:web:506621582dab103a4d08d6",
+  apiKey: "AIzaSyAGfCIZggcebtDuixtC1hGMEYsMXx_aewU",
+
+  authDomain: "e-commerce-app-daa98.firebaseapp.com",
+
+  projectId: "e-commerce-app-daa98",
+
+  storageBucket: "e-commerce-app-daa98.appspot.com",
+
+  messagingSenderId: "1028384433874",
+
+  appId: "1:1028384433874:web:b128b1bbe3651497a16f0b",
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
